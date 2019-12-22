1d905665ed4139126c16863ba04ca4fb2c55942a
Anthony Ybarra <anthony_jy72@yahoo.com>
2019-12-22 06:18:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,7 @@ def oxford_comma(array)
     return array.join
   elsif
     array.length == 2
+<<<<<<< HEAD
     return array.join(" and ")
   elsif array.length == 3
     array[-1].prepend "and "
@@ -10,6 +11,12 @@ def oxford_comma(array)
 	elsif array.length > 3
     array[-1].prepend "and "
 	array.join(", ")
+=======
+      return array.join(" and ")
+  elsif array.length == 3
+  array.replace(2, "and starfruit")
+    return array
+>>>>>>> eb18743848bbf692f2d5f872b4e6799317be9f12
   else
     print "placeholder"
   end
